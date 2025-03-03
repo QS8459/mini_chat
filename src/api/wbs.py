13e5c7289c160fb80ae0303c3b59ec113bd54282
QS8459 <sardorgpt@gmail.com>
2025-03-04 03:01:52 +0500

    Chat figured out

--- src/api/wbs.py
+++ src/api/wbs.py
@@ -8,35 +8,46 @@ from fastapi import (
     Depends,
     HTTPException
 )
-from src.core.service.account.account import get_account_service
+from src.core.service.account.account import (
+    get_account_service,
+    AccountService
+)
+from src.core.service.message.message import (
+    get_message_service,
+    MessageService
+)
 from src.core.service.auth.auth import get_current_user
 from src.core.wb_manager import manager
 from src.conf.loging import log
+from uuid import UUID
 
 
 websocket_api: APIRouter = APIRouter(prefix='/ws', tags=['ws'])
 
 
-@websocket_api.websocket('/')
+@websocket_api.websocket('/{chat_id}/')
 async def websocket_endpoint(
         websocket: WebSocket,
-        token: str = Query(...),
-        acc_service=Depends(get_account_service)
+        chat_id:UUID,
+        acc_service: AccountService = Depends(get_account_service),
+        msg_service: MessageService = Depends(get_message_service)
 
 ):
     await manager.connect(websocket)
     try:
 
         log.info("WebSocket connection established")
+        token = websocket.headers.get('Authorization').split(' ')[1]
         user = await get_current_user(token=token, acc_service=acc_service)
-        log.info(user.id)
         if user:
 
-            log.info(f"User id is {user.id}")
-
-            log.info(websocket.headers)
             data = await websocket.receive_text()
-            await manager.send_personal_message(f"You wrote nothing", websocket)
+            await msg_service.add(
+                created_by=user.id,
+                msg=data,
+                chat_id=chat_id
+            )
+            await manager.send_personal_message(f"You wrote {data}", websocket)
         else:
             raise WebSocketException(
                 reason="Authentication Error",
--- src/test.py
+++ src/test.py
@@ -3,20 +3,19 @@ import requests as req
 import jwt
 
 if __name__ == "__main__":
-    # access_token: str = ''
-    # token_req = req.post(
-    #     url='http://127.0.0.1:8000/api/v1/account/login/',
-    #     data={
-    #         'username': 'string',
-    #         'password': 'string'
-    #     }
-    # )
-    # if token_req.status_code == 200:
-    #     access_token = token_req.json().get('access_token')
-    #     print(access_token)
-    access_token: str = jwt.encode({'username': 'no_', "password": 'mini'}, algorithm="HS256", key='mini_chat')
+    access_token: str = ''
+    token_req = req.post(
+        url='http://127.0.0.1:8000/api/v1/account/login/',
+        data={
+            'username': 'string',
+            'password': 'string'
+        }
+    )
+    if token_req.status_code == 200:
+        access_token = token_req.json().get('access_token')
+        print(access_token)
     try:
-        with connect(uri=f'ws://127.0.0.1:8000/api/ws/?token={access_token}', additional_headers={"Authorization": f"Bearer {access_token}"}) as ws:
+        with connect(uri=f'ws://127.0.0.1:8000/api/ws/5e41bca3-5731-40da-b65d-4c6927e8179b/', additional_headers={"Authorization": f"Bearer {access_token}"}) as ws:
             ws.send("Hello")
             print(ws.recv())
     except Exception as e:
